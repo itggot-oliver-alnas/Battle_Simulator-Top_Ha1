5b60fb48880372517abbf42ea0a6c7aaeb1914dc
sStellar <rexstellar.pa@gmail.com>
2017-05-03 22:59:27 +0200

    Added stuff to char_gear and the compendium
    Added armor stats and shield stats (WIP) and things we agreed on the the
    compendium. Made a line in lvl_1_monster to a comment.

--- char_gear.rb
+++ char_gear.rb
@@ -1,34 +1,34 @@
 #require_relative "start_adventure.rb"
 
 def character_damage
-  @char_damage = 0
-  if @weapon == 0
-    @char_damage = 5
+  char_damage = 0
+  if weapon == 0
+    char_damage = 5
     @char_speed = 1.2
-  elsif @weapon == 1
-    @char_damage = 10
-    @char_speed = 1
-  elsif @weapon == 2
-    @char_damage = 10
+  elsif weapon == 1 or weapon == 2
+    char_damage = 10
     @char_speed = 1
   end
-
+  return char_damage
 end
-# @char_damage = 0
-#
-#   if @weapon = 0
-#
-#     @char_damage = 5
-#     @char_speed = 1.2
-#
-#   elsif @weapon = 1
-#
-#     @char_damage = 10
-#     @char_speed = 1
-#
-#   elsif @weapon = 2
-#
-#     @char_damage = 10
-#     @char_speed = 1
-#
-#   end
+
+def shield_stats
+  if shield == 0
+    #Stats and reductions (find in compendium when written)
+  elsif shield == 1
+    #Stats and reductions (find in compendium when written)
+  end
+  return shield_stats
+end
+
+def armor_stats
+  if armor == 0
+    @dmg_reduction = 0.85
+    @char_speed = @char_speed + 0.1
+  elsif armor == 1
+    @dmg_reduction = 0.7
+    @char_speed = @char_speed - 0.1
+  end
+  return dmg_reduction
+#Damage reduction depends on what shield and armor is equepped. Look in compendium for stats, then come back here and write the code. monster_damage*dmg_reduction
+end
