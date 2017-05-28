d278b5534b8c17ab57fcfcd10dc3c2d687e5911c
itggot-oliver-alnas <oliver.alnas@itggot.se>
2017-05-28 21:46:02 +0200

    many thing

--- shop_new.rb
+++ shop_new.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 def shop
   running = true
   var = 0
@@ -6,11 +8,13 @@ def shop
   choice2 = false
   gold = 200
   lines = File.readlines('shop_items.dat')
-  weapons = []
-  lines.each do |line|
-      name, damage, cost = line.split(";")
-      weapons << {"name" => name, "damage" => damage, "cost" => cost.chomp }
-  end
+  # weapons = []
+  # lines.each do |line|
+  #     unless line.include?("[")
+  #       name, damage, cost = line.split(";")
+  #       weapons << {"name" => name, "damage" => damage, "cost" => cost.chomp }
+  #     end
+  # end
   puts "
 Welcome to the general store!"
   while running
@@ -25,38 +29,19 @@ Welcome to the general store!"
       end
     end
 
-    case lines
-    when "[Shop Categories]"
-      lines.each do |cat, index|
-        puts "[#{index}] #{cat}"
-
+    items = {}
+    key = "default"
+    lines.each do |line|
+      line.chomp!
+      if line[0] == "["
+        key = line.gsub("[","").gsub("]","")
+        items[key] = []
+      else
+        items[key] << line
       end
-
     end
 
-    weapons.each_with_index do |weapon, index|
-      puts "[#{index}] #{weapon["name"]}"
-    end
-
-    # puts "[#{var += 1}] Shields [ X ]"
-    # puts "[#{var += 1}] Armor [ X ]"
-    # puts "[#{var += 1}] Cancel [ X ]"
-    var = 0
-   input = gets.chomp.to_i
-    system"cls"
-      weapons.each_with_index do |weapon, index|
-        print "[#{index}]"
-        print weapon["name"]
-        print ": " + weapon["damage"]
-        print " Damage "
-        #print " Cost: "
-        print weapon["cost"]
-        puts " gold"
-      end
-
-    end
   end
-
 end
 
 shop
